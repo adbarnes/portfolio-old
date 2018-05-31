03b11118449d70aaf9647a587aa36ef38fb903d6
adbarnes <adbarnes@gmail.com>
2018-05-31 20:32:29 +0100

    test

--- showHide.js
+++ showHide.js
@@ -3,7 +3,7 @@ var $btns = $('.btn').click(function() {
     $('#parent > div').fadeIn(450);
   } else {
     var $el = $('.' + this.id).fadeIn(450);
-    $('#parent > div').not($el).hide();
+    $('#parent > div').not($el).css("cssText", "display: none !important;");
   }
   $btns.removeClass('active');
   $(this).addClass('active');
